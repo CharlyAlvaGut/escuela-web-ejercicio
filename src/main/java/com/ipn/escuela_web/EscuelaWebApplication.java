92c4292bc36d75087514f30c1d5a932f8b2b8793
Jose Carlos Alvarado Gutierrez <jcagutierrez@pbi>
2026-04-23 20:35:28 -0600

    Subida inicial del proyecto.

---
+++ src/main/java/com/ipn/escuela_web/EscuelaWebApplication.java
@@ -0,0 +1,13 @@
+package com.ipn.escuela_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscuelaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscuelaWebApplication.class, args);
+	}
+
+}
